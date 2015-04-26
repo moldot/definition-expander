c75d490609ad25fb64db80390a0dee8582854f34
moldot <molev.mt@gmail.com>
2015-04-26 11:36:30 -0400

    initial commit

---
+++ def-tree.py
@@ -0,0 +1,2 @@
+from maps import *
+from wordnet.corpus import wordnet as wn
---
+++ maps.py
@@ -0,0 +1,8 @@
+import json
+
+def_to_inf = json.load(open('maps/def_to_inf.json', 'r'))
+inf_to_def = json.load(open('maps/inf_to_def.json', 'r'))
+pages_info = json.load(open('maps/pages_info.json', 'r'))
+
+sorted_def = sorted(def_to_inf.items(), key=lambda x: -x[1]['count'])
+sorted_inf = sorted(inf_to_def.items(), key=lambda x: -x[1]['count'])
